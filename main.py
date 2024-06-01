3b564ee73899f6559f873a82f206304441d848c9
Radu Marias <radumarias@gmail.com>
2024-06-02 01:34:45 +0300

    sample

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import zeroize
 
 arr = bytearray(b'1234567890')
 zeroize.zeroize1(arr)
+print("zeroize'ing arr...: ")
+print("checking if arr is zeroized...")
 assert arr == bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
 print("all good, bye!")
