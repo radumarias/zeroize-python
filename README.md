5f8274425b530bc03d90e0bd07a17b6935e4c212
Radu Marias <radumarias@gmail.com>
2024-06-02 03:49:51 +0300

    doc

--- README.md
+++ README.md
@@ -19,12 +19,15 @@ arr = bytearray(b'1234567890')
 arr_np = np.array([0] * 10, dtype=np.uint8)
 arr_np[:] = arr
 assert arr_np.tobytes() == b'1234567890'
+
+print("zeroize'ing...: ")
 zeroize.zeroize1(arr)
 zeroize.zeroize_np(arr_np)
-print("zeroize'ing...: ")
+
 print("checking if is zeroized...")
 assert arr == bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
 assert all(arr_np == 0)
+
 print("all good, bye!")
 ```
 # Building from source
--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import zeroize
 import numpy as np
 
 
+# regular array
 arr = bytearray(b'1234567890')
+
+# numpy array
 arr_np = np.array([0] * 10, dtype=np.uint8)
 arr_np[:] = arr
 assert arr_np.tobytes() == b'1234567890'
+
+print("zeroize'ing...: ")
 zeroize.zeroize1(arr)
 zeroize.zeroize_np(arr_np)
-print("zeroize'ing...: ")
+
 print("checking if is zeroized...")
 assert arr == bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
 assert all(arr_np == 0)
+
 print("all good, bye!")
