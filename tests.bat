b07cd3ffad0928daaa29ddca5bae4c556222d40d
Radu Marias <radumarias@gmail.com>
2024-06-22 15:19:56 +0300

    Update tests.bat

--- tests.bat
+++ tests.bat
@@ -1,8 +1,8 @@
 pytest
-if %errorlevel% neq 0 exit /b %errorlevel%
+if ($LASTEXITCODE) { exit $LASTEXITCODE }
 
 python examples/lock_and_zeroize.py
-if %errorlevel% neq 0 exit /b %errorlevel%
+if ($LASTEXITCODE) { exit $LASTEXITCODE }
 
 python examples/zeroize_before_fork.py
-if %errorlevel% neq 0 exit /b %errorlevel%
+if ($LASTEXITCODE) { exit $LASTEXITCODE }
