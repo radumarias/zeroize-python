3b30e4fd7ba9cd26e30021b96471a746cb6c3962
radu <radu@appscenic.com>
2024-06-23 08:00:35 +0300

    tests

--- test.bat
+++ test.bat
@@ -3,8 +3,5 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 python examples/lock_and_zeroize.py
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-python examples/zeroize_before_fork.py
-if %errorlevel% neq 0 exit /b %errorlevel%
-
 python examples/mlock.py
 if %errorlevel% neq 0 exit /b %errorlevel%
