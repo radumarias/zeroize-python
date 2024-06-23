0c1a52851c048a51efbd4afd9bfc1385ae793693
radu <radu@appscenic.com>
2024-06-23 04:03:42 +0300

    testing mlock

--- src/lib.rs
+++ src/lib.rs
@@ -108,8 +108,8 @@ fn as_array<'a>(arr: &'a Bound<PyAny>, py: Python<'a>) -> PyResult<&'a [u8]> {
 /// Calls the platform's underlying `mlock(2)` implementation.
 unsafe fn _mlock(ptr: *mut u8, len: usize) -> bool {
     // memsec::mlock(ptr, len)
-    // region::lock(ptr, len).is_ok()
-    libsodium_sys::sodium_mlock(ptr as *mut libc::c_void, len) == 0
+    region::lock(ptr, len).is_ok()
+    // libsodium_sys::sodium_mlock(ptr as *mut libc::c_void, len) == 0
 }
 
 /// Calls the platform's underlying `munlock(2)` implementation.
