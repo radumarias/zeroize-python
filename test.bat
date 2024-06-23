04fc8430b738d1b4d22652371a96d74ff2c6c1d1
Radu Marias <radumarias@gmail.com>
2024-06-23 07:51:23 +0300

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,11 +1,11 @@
-python examples/mlock.py
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
-
 pytest
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
+if ($LASTEXITCODE) exit $LASTEXITCODE
 
 python examples/lock_and_zeroize.py
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
+if ($LASTEXITCODE) exit $LASTEXITCODE
 
 python examples/zeroize_before_fork.py
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
+if ($LASTEXITCODE) exit $LASTEXITCODE
+
+python examples/mlock.py
+if ($LASTEXITCODE) exit $LASTEXITCODE
